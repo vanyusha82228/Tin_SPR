c39bf19f34816472a24f926be7f1e9e01949ca54
vanusha <ivan00.02mail.ru>
2024-03-04 19:53:45 +0300

    fix milsec

--- scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduler/LinkUpdaterScheduler.java
@@ -1,7 +1,5 @@
 package edu.java.scheduler;
 
-import edu.java.github.GitHubWebClient;
-import edu.java.stackocerflow.StackOverflowWebClient;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,16 +9,9 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableScheduling
 public class LinkUpdaterScheduler {
-    private final GitHubWebClient gitHubWebClient;
-    private final StackOverflowWebClient stackOverflowWebClient;
-    private final static int ID = 123;
 
-    public LinkUpdaterScheduler(GitHubWebClient gitHubWebClient, StackOverflowWebClient stackOverflowWebClient) {
-        this.gitHubWebClient = gitHubWebClient;
-        this.stackOverflowWebClient = stackOverflowWebClient;
-    }
 
-    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    @Scheduled(fixedDelayString = "${app.scheduler.interval.toMillis()}")
     public void update() {
 
         log.info("Обновление ссылок...");
