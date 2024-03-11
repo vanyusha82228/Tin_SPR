701b2b78dd3923e984c6d7e66ee8fcfc09a5c8b6
vanusha <ivan00.02mail.ru>
2024-03-11 22:42:53 +0300

    add controllers test

---
+++ bot/src/test/java/edu/java/bot/controller/ChatControllerTest.java
@@ -0,0 +1,35 @@
+package edu.java.bot.controller;
+
+
+import edu.java.bot.controllers.ChatController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ChatController.class)
+public class ChatControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testRegisterChat() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/tg-chat/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteChat() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/tg-chat/{id}", 123L)
+                .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+}
+
+
