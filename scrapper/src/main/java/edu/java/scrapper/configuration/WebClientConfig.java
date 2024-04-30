4fba8920e7386bbfd82d92dc0fb41b261449aeb5
Ivan <ivan00.02@mail.ru>
2024-04-30 13:43:28 +0300

    init hw6

--- scrapper/src/main/java/edu/java/scrapper/configuration/WebClientConfig.java
+++ scrapper/src/main/java/edu/java/scrapper/configuration/WebClientConfig.java
@@ -6,6 +6,7 @@ import java.net.URI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class WebClientConfig {
 
